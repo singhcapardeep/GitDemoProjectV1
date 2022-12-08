9a584bd7bd59790f64e4d6b4878febb7e1df0160
singhcapardeep <singh.ca.pardeep@gmail.com>
2022-12-08 11:30:43 -0800

    Test3 file commit

---
+++ src/Test3.java
@@ -0,0 +1,21 @@
+public class Test3 {
+	
+	public static void main (String[] args)
+	{
+		
+		String hungry = "Yes";
+		
+		if (hungry == "Yes")
+		{
+			System.out.println("eat somthing...............");
+			
+		}
+		else
+			
+		{
+			System.out.println("Do work...............");
+			
+		}
+	}
+
+}
